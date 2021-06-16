c367269ad836b7a056efe84b7b76b7592e57fa74
BZIvanov <ivanov.biser@gmx.com>
2021-06-16 18:35:33 +0300

    demo_c fixed logic to query all comments for all posts

--- demo-c/index.js
+++ demo-c/index.js
@@ -3,25 +3,14 @@ const express = require('express');
 require('./db');
 
 const Post = require('./db/models/post');
-const Comment = require('./db/models/comment');
 
 const app = express();
 
 app.use(express.json());
 
-// app.get('/', async (req, res) => {
-//   try {
-//     const comments = await Post.query();
-
-//     res.status(200).json({ comments });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
 app.get('/', async (req, res) => {
   try {
-    const postsComments = await Comment.query().withGraphFetched('[comments]');
+    const postsComments = await Post.query().withGraphFetched('[comments]');
 
     res.status(200).json({ postsComments });
   } catch (err) {
